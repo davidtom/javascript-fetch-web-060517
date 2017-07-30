86ccc552b2075d720a24a915804b759e808fd273
davidtom <davidjtomczyk@gmail.com>
2017-07-30 14:19:30 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+const token = "df2e25748b4bb544105b2ec1d9a69ee0b5ed8709"
+
+fetch("https://api.github.com/user/repos",
+  {headers:
+    {Authorization: `token ${token}`}
+  }
+  ).
+  then(res => res.json()).
+  then(json => console.log(json))
